b019ae9940a95a9f46d4d07eba84091037b7d23b
Omar-Ashinawy <omarashinawy@gmail.com>
2020-07-09 21:23:42 +0200

    Removed transparancy.js

--- js/transparency.js
+++
@@ -1,8 +0,0 @@
-let transp = document.getElementById("transp");
-let btn = document.querySelector(".main-btn");
-
-alphaValue = transp.textContent;
-btn.addEventListener("click", function () {
-
-    console.log(alphaValue);
-})
